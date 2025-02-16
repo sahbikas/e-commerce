07cb5cdb32b64afe49a2b908f9aa6a7f40b6ea36
sahbikas <sahbikash301@gmail.com>
2025-02-16 20:53:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+
+## state and props 
+### props
+ -> the incoming data to a react 
+ -> Readonly value
+ -> any datatype in props
+ -> as an attribute of the component
+ -> Props are always object data type
+
+
+ ### web Hook
+
+ - special functions in react to acheive some specifc features of react
+ - for example
+     -> state hook
+     -> effect hook
+     -> reducer hook
+     -> performace hook
+     -> custom hook
+     -> Every hook functions start with  use 
---
+++ src/Sidebar/Colors/Colors.jsx
@@ -0,0 +1,51 @@
+import Input from "../../components/Input";
+import "./Colors.css";
+
+function Colors({handleChange}) {
+  return (
+    <div >
+      <h2 className="sidebar-title color-title">Color</h2>
+    
+      <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test2"/>
+          <span className="checkmark all"></span>All
+        </label>
+
+ <Input
+  handleChange={handleChange}
+  value="black"
+  title="Black"
+  name="test1"
+  color="black"
+ />
+ 
+ <Input
+  handleChange={handleChange}
+  value="blue"
+  title="Blue"
+  name="test1"
+  color="blue"
+ />
+ <Input
+  handleChange={handleChange}
+  value="green"
+  title="Green"
+  name="test1"
+  color="green"
+ />
+ <Input
+  handleChange={handleChange}
+  value="red"
+  title="Red"
+  name="test1"
+  color="red"
+ />
+ <label className="sidebar-label-container">
+  <input type="radio" onChange={handleChange} value="white" name="test1"/>
+  <span className="checkmark" style={{backgroundColor: "white", border: "2px solid balck"}}></span>White
+ </label>
+  </div>
+  )
+}
+
+export default Colors
